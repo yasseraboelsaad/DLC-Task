cb9a1414e0ff429eb2b3f155943ca744a868889b
Yasser Aboel-Saad <yasser.aboelsaad@gmail.com>
2017-08-01 02:08:13 +0200

    Initialized project

---
+++ DLC-Task/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DLC-Task
+//
+//  Created by Yasser Abouel-Saad on 1/8/17.
+//  Copyright © 2017 Yasser Abouel-Saad. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
