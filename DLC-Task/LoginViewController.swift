cfcc3e2d54b64ca635b4566bc9cdc113ca8ffac1
Yasser Aboel-Saad <yasser.aboelsaad@gmail.com>
2017-08-01 15:10:25 +0200

    added login view

--- DLC-Task/LoginViewController.swift
+++ DLC-Task/LoginViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet var loginWithFbButton: UIButton!
+    @IBOutlet var signInButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        signInButton.layer.cornerRadius = 25
+        signInButton.layer.borderWidth = 1
+        signInButton.layer.borderColor = UIColor.clear.cgColor
+        
+        loginWithFbButton.layer.cornerRadius = 25
+        loginWithFbButton.layer.borderWidth = 1
+        loginWithFbButton.layer.borderColor = UIColor.clear.cgColor
     }
 
     override func didReceiveMemoryWarning() {
